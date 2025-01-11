72e4e15f5a406e3f81db723f6ada10ae90f1011e
AdityaSawant2505 <adityasawant5814@gmail.com>
2025-01-11 17:44:39 +0530

    new with email

---
+++ DLL/Implementations/EmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DLL.Interfaces;
+
+namespace DLL.Implementations
+{
+    public class EmailService:IEmailService
+    {
+
+    }
+}
